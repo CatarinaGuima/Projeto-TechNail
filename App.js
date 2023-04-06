b06a703049130dc5f8116c570b672f7c8922e213
Catarina Guimarães <110750750+CatarinaGuima@users.noreply.github.com>
2023-04-06 09:59:13 -0300

    second commit

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Image, FlatList, TouchableOpacity, Button, ScrollView, TextInput, Alert } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList, TouchableOpacity, Button, TextInput, Alert } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 
 export default function App() {
 
   const [lista, setLista] = useState([
-    { uri: 'https://cdn-icons-png.flaticon.com/512/3789/3789889.png', name: 'Nails' },
-    { uri: 'https://cdn-icons-png.flaticon.com/512/942/942759.png', name: 'Appointment' },
-    { uri: 'https://cdn-icons-png.flaticon.com/512/1375/1375106.png', name: 'Gallery' },
-    { uri: 'https://cdn-icons-png.flaticon.com/512/2225/2225218.png', name: 'Prices' },
-    { uri: 'https://cdn-icons-png.flaticon.com/512/2830/2830514.png', name: 'Employees' },
-    { uri: 'https://cdn1.iconfinder.com/data/icons/creative-process-14/512/Review-512.png', name: 'Reviews' },
+    { id: 1, uri: 'https://cdn-icons-png.flaticon.com/512/3789/3789889.png', name: 'Nails' },
+    { id: 2, uri: 'https://cdn-icons-png.flaticon.com/512/942/942759.png', name: 'Appointment' },
+    { id: 3, uri: 'https://cdn-icons-png.flaticon.com/512/1375/1375106.png', name: 'Gallery' },
+    { id: 4, uri: 'https://cdn-icons-png.flaticon.com/512/2225/2225218.png', name: 'Prices' },
+    { id: 5, uri: 'https://cdn-icons-png.flaticon.com/512/2830/2830514.png', name: 'Employees' },
+    { id: 6, uri: 'https://cdn1.iconfinder.com/data/icons/creative-process-14/512/Review-512.png', name: 'Reviews' },
   ])
 
   const [change, setChange] = useState('Register')
@@ -20,26 +20,60 @@ export default function App() {
     setChange('Registered');
   }
 
+  function HeaderComponent() {
+    return (
+      <>
+        <View style={styles.header}>
+          <Image
+            style={styles.logo}
+            source={require('./assets/Images/logo2.png')}
+          ></Image>
+        </View>
+        <Text style={styles.hello}>Hello,</Text>
+        <Text style={styles.category}>Select your category</Text>
+        <View style={styles.look}>
+          <View>
+            <Entypo name="magnifying-glass" size={28} color="black" />
+          </View>
+          <View style={styles.search}>
+            <TextInput style={styles.search} placeholder='Buscar...'></TextInput>
+          </View>
+        </View>
+      </>
+    )
+  }
+
+  function FooterComponent() {
+    return (
+      <>
+        <View style={styles.button}>
+          <Button
+          title={change}
+          color='#8AEAE5'
+          style={styles.register}
+          onPress={clicarBotao}
+          />
+          <View></View>
+          <Button
+          title='Login'
+          color='#E33488'
+          style={styles.login}
+          onPress={() => Alert.alert('Login complete!')} />
+          <Text style={styles.legend}>Or login with Social Media</Text>
+        </View>
+      </>
+    )
+  }
+
   return (
 
-    <ScrollView style={styles.container}>
-      <View style={styles.header}>
-        <Image
-          style={styles.logo}
-          source={require('./assets/Images/logo2.png')}
-        ></Image>
-      </View>
-      <Text style={styles.hello}>Hello,</Text>
-      <Text style={styles.category}>Select your category</Text>
-      <View style={styles.look}>
-        <Entypo name="magnifying-glass" size={28} color="black" />
-        <TextInput style={styles.search} placeholder='Buscar...'></TextInput>
-      </View>
+    <View style={styles.container}>
       <FlatList
         style={styles.flatlist}
         numColumns={3}
-        keyExtractor={(item) => item.number}
+        keyExtractor={(item) => item.id}
         data={lista}
+        ListHeaderComponent={HeaderComponent}
         renderItem={({ item }) => (
           <View style={styles.icons}>
             <TouchableOpacity>
@@ -52,31 +86,17 @@ export default function App() {
             <Text style={styles.name}>{item.name}</Text>
           </View>
         )}
+        ListFooterComponent={FooterComponent}
       />
-      <View style={styles.button}>
-        <Button
-          title={change}
-          color='#8AEAE5'
-          style={styles.register}
-          onPress={clicarBotao}
-        />
-        <Text></Text>
-        <Button
-          title='Login'
-          color='#E33488'
-          style={styles.login}
-          onPress={() => Alert.alert('Login complete!')} />
-        <Text style={styles.legend}>Or login with Social Media</Text>
-        <StatusBar style="auto" /></View>
-    </ScrollView>
-  );
-}
+      <StatusBar style="auto" />
+    </View>
+  )
 
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFF0F5',
-
   },
   flatlist: {
     flex: 1,
@@ -149,7 +169,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     width: 180,
     height: 38,
-    borderRadius: 100 / 6,
     marginLeft: 5,
+    borderRadius: 100 / 6,
   },
 });
